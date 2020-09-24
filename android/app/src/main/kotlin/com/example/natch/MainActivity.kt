21e02174ca454ec11a695014511d21a0569135cd
amitchaudhari9121 <amitchaudhari9121@gmail.com>
2020-09-24 18:48:41 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/natch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.natch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,201 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SerializedForm(),
+    );
+  }
+}
+
+//defined 3 fields which create a serialized form, that can be submitted as json.
+class SerializedFormBloc extends FormBloc<String, String> {
+  final name = TextFieldBloc(
+    name: 'name',
+  );
+
+  final gender = SelectFieldBloc(
+    name: 'gender',
+    items: ['male', 'female'],
+  );
+
+
+  final birthDate = InputFieldBloc<DateTime, Object>(
+    name: 'birthDate',
+    toJson: (value) => value.toUtc().toIso8601String(),
+  );
+
+  final photo = 
+
+  SerializedFormBloc() {
+    addFieldBlocs(
+      fieldBlocs: [
+        name,
+        gender,
+        birthDate,
+      ],
+    );
+  }
+
+  @override
+  void onSubmitting() async {
+    emitSuccess(
+      canSubmitAgain: false,
+      successResponse: JsonEncoder.withIndent('    ').convert(
+        state.toJson(),
+      ),
+    );
+  }
+}
+
+class SerializedForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => SerializedFormBloc(),
+      child: Builder(
+        builder: (context) {
+          final formBloc = context.bloc<SerializedFormBloc>();
+
+          return Theme(
+            data: Theme.of(context).copyWith(
+              inputDecorationTheme: InputDecorationTheme(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            child: Scaffold(
+              resizeToAvoidBottomInset: false,
+              appBar: AppBar(
+                title: Text('Sign Up'),
+                centerTitle: true,
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: formBloc.submit,
+                child: Icon(Icons.send),
+              ),
+              body: FormBlocListener<SerializedFormBloc, String, String>(
+                onSuccess: (context, state) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text(state.successResponse),
+                    duration: Duration(seconds: 2),
+                  ));
+                },
+                child: SingleChildScrollView(
+                  physics: ClampingScrollPhysics(),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: <Widget>[
+                        TextFieldBlocBuilder(
+                          textFieldBloc: formBloc.name,
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                            prefixIcon: Icon(Icons.people),
+                          ),
+                        ),
+                        Center(
+                          child: RadioButtonGroupFieldBlocBuilder<String>(
+                            selectFieldBloc: formBloc.gender,
+                            itemBuilder: (context, value) =>
+                                value[0].toUpperCase() + value.substring(1),
+                            decoration: InputDecoration(
+                              labelText: 'Gender',
+                              prefixIcon: SizedBox(),
+                            ),
+                          ),
+                        ),
+                        DateTimeFieldBlocBuilder(
+                          dateTimeFieldBloc: formBloc.birthDate,
+                          format: DateFormat('yyyy-mm-dd'),
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1950),
+                          lastDate: DateTime.now(),
+                          decoration: InputDecoration(
+                            labelText: 'Date of Birth',
+                            prefixIcon: Icon(Icons.calendar_today),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class LoadingDialog extends StatelessWidget {
+  static void show(BuildContext context, {Key key}) => showDialog<void>(
+        context: context,
+        useRootNavigator: false,
+        barrierDismissible: false,
+        builder: (_) => LoadingDialog(key: key),
+      ).then((_) => FocusScope.of(context).requestFocus(FocusNode()));
+
+  static void hide(BuildContext context) => Navigator.pop(context);
+
+  LoadingDialog({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Center(
+        child: Card(
+          child: Container(
+            width: 80,
+            height: 80,
+            padding: EdgeInsets.all(12.0),
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SuccessScreen extends StatelessWidget {
+  SuccessScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.tag_faces, size: 100),
+            SizedBox(height: 10),
+            Text(
+              'Success',
+              style: TextStyle(fontSize: 54, color: Colors.black),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            RaisedButton.icon(
+              onPressed: () => Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (_) => SerializedForm())),
+              icon: Icon(Icons.replay),
+              label: Text('AGAIN'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
