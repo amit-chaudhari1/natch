e2221add3b03e83fe962a330544f6d4d9b301786
amitchaudhari9121 <amitchaudhari9121@gmail.com>
2020-10-16 12:01:36 +0530

    buildpassing

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,20 @@
-import 'dart:async';
+import 'package:chill/bloc/authentication/authentication_bloc.dart';
+import 'package:chill/bloc/blocDelegate.dart';
+import 'package:chill/repositories/userRepository.dart';
+import 'package:chill/ui/pages/home.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-//import swiper. or whateven  i name it.
-import 'interfaces/registration_UI.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(new MaterialApp(
-      home: new SplashScreen(),
-      routes: <String, WidgetBuilder>{
-        //'/swiping': (BuildContext context) => new someshit(),
-        '/registration': (BuildContext context) => new TextForm()
-      },
-    ));
+import 'bloc/authentication/authentication_event.dart';
 
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => new _SplashScreenState();
-}
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final UserRepository _userRepository = UserRepository();
 
-class _SplashScreenState extends State<SplashScreen> {
-  startTime() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool firstTime = prefs.getBool('first_time');
+  BlocSupervisor.delegate = SimpleBlocDelegate();
 
-    var _duration = new Duration(seconds: 3);
-
-    if (firstTime != null && !firstTime) {
-      // Not first time
-      return new Timer(_duration, navPageSwipe);
-    } else {
-      // First time
-      prefs.setBool('first_time', false);
-      return new Timer(_duration, navPageRegister);
-    }
-  }
-
-  void navPageSwipe() {
-    //Navigator.of(context).pushReplacementNamed('/swiping');
-  }
-
-  void navPageRegister() {
-    Navigator.of(context).pushReplacementNamed('/registration');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    startTime();
-  }
-
-  //splash widget.
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Center(
-        child: new Image.asset(
-          //add splashback image in assets in pubspec.yaml
-          'assets/images/Splash.jpg',
-          width: size.width,
-          height: size.height,
-          fit: BoxFit.fill,
-        ),
-      ),
-    );
-  }
+  runApp(BlocProvider(
+      create: (context) => AuthenticationBloc(userRepository: _userRepository)
+        ..add(AppStarted()),
+      child: Home(userRepository: _userRepository)));
 }
